402721f0fd1f29b1b28e2f29910ea647feb3c5b5
Akachuk <gabrielnweke40@gmail.com>
2022-09-08 16:36:07 -0700

    Gabriel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+{
 /**
  *  * main - texto
  *   *
@@ -7,7 +7,7 @@
  *     */
 
 int main(void)
-{
+
 			printf("with proper grammar, but the outcome is a piece of art,\n");
 						return (0);
 }
