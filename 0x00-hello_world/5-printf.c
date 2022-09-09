bad4f4768b07966be34dd97f03950169c725a2bc
Akachuk <gabrielnweke40@gmail.com>
2022-09-08 17:08:08 -0700

    Gabriel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
  1  #include <stdio.h>
  2  
- 3  /**
- 4   * main - texto
- 5   *
- 6   * Return: text in printf
- 7   */
- 8  int main(void)
+ 3   /**
+ 4    * main - texto
+ 5    *
+ 6    * Return: text in printf
+ 7    */
+ 8   int main(void)
  9  
- 10 {
+ 10  {
  11	  	printf("with proper grammar, but the outcome is a piece of art,\n");
- 12			return (0);
- 13 }
+ 12 			return (0);
+ 13  }
