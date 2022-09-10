927e848e2fbc7113f07a4c047993eea22fd091ca
Akachuk <gabrielnweke40@gmail.com>
2022-09-10 12:41:37 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this prints alphabet in lowcase
+ *
+ * Return: always return 0
+ *
+ */
+int main (void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	putchar(a);
+	putchar('n\');
+	return (0)
+}
