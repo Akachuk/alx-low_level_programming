c6c33d4ee8a878d5e7d9adc88647c24cf46a33c1
Akachuk <gabrielnweke40@gmail.com>
2022-09-10 19:30:00 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (a = 'a'; a <= 'z'; a++);
 	{
-		putchar(a);
+	putchar(a);
 	}
 	for (b = 'A'; b <= 'z' b++)
 	{
-		putchar(b);
+	putchar(b);
 	}
 	putchar('\n');
 	return (0);
