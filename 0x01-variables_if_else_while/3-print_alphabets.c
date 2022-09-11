a57241e6e088530cb1388e88f3a8940bb551a848
Akachuk <gabrielnweke40@gmail.com>
2022-09-10 19:19:45 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this prints alphabet in lower and uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A'
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'z' b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
